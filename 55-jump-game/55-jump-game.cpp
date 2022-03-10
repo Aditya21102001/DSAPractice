abc6d1cc4efe824728c7606095baf3ba77602656
Aditya Yadav <54741890+Aditya21102001@users.noreply.github.com>
2022-03-11 01:23:32 +0530

    Time: 64 ms (86.07%), Space: 48.4 MB (30.78%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int l=n-1;
+        for(int i=n-2; i>=0; i--)
+        {
+            if(i+nums[i]>=l)
+            {
+                l=i;
+            }
+        }
+        return l==0;
+    }
+};
