9b5b072f12e8c3a03f68433fd049a4b74c6564d4
Aditya Yadav <54741890+Aditya21102001@users.noreply.github.com>
2022-03-22 19:34:37 +0530

    Time: 7 ms (6.63%), Space: 6 MB (45.22%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
+    double mp(double x, long long n)
+    {
+        if(n==0)
+            return (double)1.0;
+        if(n%2==0)
+            return myPow(double(x*x), n/2);
+        else
+            return (double)x*(myPow(x, n-1));
+    }
     double myPow(double x, int n) {
         long long p=n;
         if(n<0)
-            p=p*(-1);
-        double ans=1;
-        while(p>0)
-        {
-            if(p%2==0)
-            {
-                x=(x*x);
-                p=p/2;
-            }
-            else
-            {
-                ans=ans*x;
-                p=p-1;
-            }
-        }
+            p=-1*p;
+        double ans=mp(x, p);
         if(n<0)
             ans=double(1.0)/ans;
         return ans;
