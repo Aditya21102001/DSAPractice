bdd49df42b8d6d74f740e61d70828e525aa4834a
Aditya Yadav <54741890+Aditya21102001@users.noreply.github.com>
2022-05-09 01:23:37 +0530

    Time: 3 ms (61.14%), Space: 6.2 MB (88.81%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -0,0 +1,86 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int cn=0;
+        char c;
+        unordered_map<char, int> mp;
+        for(int i=0; i<s.length(); i++)
+        {
+            mp[s[i]]++;
+        }
+        for(auto it=mp.begin(); it!=mp.end(); it++)
+        {
+            if(cn<=it->second)
+            {
+                cn=it->second;
+                c=it->first;
+            }
+        }
+        if(cn>(s.length()+1)/2)
+        {
+            return "";
+        }
+        int i;
+        string ans="";
+        for(i=0; i<s.length(); i++)
+        {
+            if(cn<=0)
+            {
+                break;
+            }
+            if(i%2==0)
+            {
+                ans+=c;
+                cn--;
+            }
+            else
+            {
+                ans+='1';
+            }
+        }
+        int j=1;
+        for(auto it=mp.begin(); it!=mp.end(); it++)
+        {
+            if(it->first==c)
+                continue;
+            for(int k=0; k<it->second; k++)
+            {
+                if(i<s.length())
+                {
+                    if(i<s.length() && i%2==0)
+                    {
+                        ans+=it->first;
+                        i++;
+                    }
+                    else
+                    {
+                        ans+='1';
+                        i++;
+                        if(i<s.length())
+                        {
+                            ans+=it->first;
+                            i++;
+                        }
+                        else
+                        {
+                            if(j%2!=0)
+                            {
+                                ans[j]=it->first;
+                                j+=2;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if(j%2!=0)
+                    {
+                        ans[j]=it->first;
+                        j+=2;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
